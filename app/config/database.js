f524cc875b18e5e76be43799af37a1a990756824
Unknown <favier.alexis.96@gmail.com>
2017-12-26 21:58:33 +0100

    Mise en place du logger et d'un fichier de conf pour la bdd

--- app/config/database.js
+++ app/config/database.js
@@ -1,11 +1,17 @@
-/** On déclare la configuration */
+/** On importe les librairies */
 const mysql = require('mysql');
+const ini = require('ini');
+const fs = require('fs');
 
+/** On charge la config **/
+const config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'));
+
+/** On crée la connexion **/
 const databaseConfig = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "gout"
+    host: config.database.host,
+    user: config.database.user,
+    password: config.database.password,
+    database: config.database.database,
 });
 
 
