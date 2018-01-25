86a4e5d17aa8817337ec9f404451ee8e7a292175
a-favier <favier.alexis.96@gmail.com>
2018-01-25 09:14:17 +0100

    Ajout d'une demande de l'ancien mdp quand on change le mdp

---
+++ app/middlewares/validPasswordMiddleware.js
@@ -0,0 +1,22 @@
+/** On importe les librairies */
+const bcrypt = require('bcrypt-nodejs');
+
+/** On importe les modèles */
+const User = require('../models/user');
+
+/** On créée le Middleware */
+const validPasswordMiddleware = (req, res, next) => {
+    const pseudo = req.params.pseudo;
+    const oldPassword = req.body.old;
+
+    User.getPasswordByPseudo(pseudo, (err, user) => {
+        if(bcrypt.compareSync(oldPassword, user[0].password)){
+            next();
+        }else{
+            res.status(401).send([{ success: false, message: 'Wrong password'}]);
+        }
+    });
+};
+
+/** On exporte le Middleware */
+module.exports = validPasswordMiddleware;
